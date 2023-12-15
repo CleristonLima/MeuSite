9739d53f680938d9fada5cbd5adf69ea000d922f
Cleriston Lima <87100328+CleristonLima@users.noreply.github.com>
2023-12-15 17:34:13 -0300

    Delete Meu Portifolio directory

--- Meu Portifolio/Sites_Sistemas.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    var menu = document.querySelector('ul');
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
-    var menuIcon = document.querySelector('.menu-icon');
-    menuIcon.classList.toggle('open');
-}
